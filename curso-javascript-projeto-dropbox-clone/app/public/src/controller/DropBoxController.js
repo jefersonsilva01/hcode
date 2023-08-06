301dfaf249c93333d047060a705208b66c3375c6
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2023-08-06 18:31:32 -0300

    Update upload

--- curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
+++ curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
@@ -18,25 +18,9 @@ class DropBoxController {
 
         this.initEvents();
         this.readFiles();
-        
+
         connectFirebase();
     }
-
-    // connectFirebase() {
-    //     const firebaseConfig = {
-    //         apiKey: "AIzaSyBYs4M-bfAuofkl_irtwXrdgdPvEMGNSRs",
-    //         authDomain: "dropbox-clone-c4766.firebaseapp.com",
-    //         databaseURL: "https://dropbox-clone-c4766-default-rtdb.firebaseio.com",
-    //         projectId: "dropbox-clone-c4766",
-    //         storageBucket: "dropbox-clone-c4766.appspot.com",
-    //         messagingSenderId: "977961143569",
-    //         appId: "1:977961143569:web:61fc18cedd45ae003d10b0",
-    //         measurementId: "G-DMHM6KMDCG"
-    //     };
-
-    //     // Initialize Firebase
-    //     firebase.initializeApp(firebaseConfig);
-    // }
 
     getSelection() {
         return this.listFilesEl.querySelectorAll('.selected');
