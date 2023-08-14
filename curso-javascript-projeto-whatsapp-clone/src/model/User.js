baafa4f09b3dc106a047b15283461af444904aee
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2023-08-14 00:13:22 -0300

    Função enviar contato

--- curso-javascript-projeto-whatsapp-clone/src/model/User.js
+++ curso-javascript-projeto-whatsapp-clone/src/model/User.js
@@ -7,24 +7,27 @@ export class User extends Model {
 
         super();
 
-        if(id) this.getById(id);
+        if (id) this.getById(id);
     }
 
-    get name() { return this._data.name; }
-    set name(value) { this._data.name = value; }
+    get name() { return this._data.name }
+    set name(value) { this._data.name = value }
 
-    get email() { return this._data.email; }
-    set email(value) { this._data.email = value; }
+    get email() { return this._data.email }
+    set email(value) { this._data.email = value }
 
     get photo() { return this._data.photo; }
-    set photo(value) { this._data.photo = value; }
+    set photo(value) { this._data.photo = value }
+
+    get chatId() { return this._data.chatId; }
+    set chatId(value) { this._data.chatId = value }
 
     getById(id) {
         return new Promise((s, f) => {
 
             User.findByEmail(id).onSnapshot(doc => {
                 this.fromJSON(doc.data());
-                s(doc);                
+                s(doc);
             });
         });
     }
@@ -34,20 +37,44 @@ export class User extends Model {
     }
 
     static getRef() {
-        
+
         return Firebase.db().collection('/users');
     }
 
+    static getContactsRef(id) {
+        return User.getRef()
+            .doc(id)
+            .collection('contacts');
+    }
+
     static findByEmail(email) {
-        
+
         return User.getRef().doc(email);
     }
 
-    addContact(contact) {        
-        return User.getRef()
-            .doc(this.email)
-            .collection('contacts')
+    addContact(contact) {
+        return User.getContactsRef(this.email)
             .doc(btoa(contact.email))
             .set(contact.toJSON());
+    }
+
+    getContacts(filter = '') {
+        return new Promise((s, f) => {
+            User.getContactsRef(this.email).where('name', '>=', filter).onSnapshot(docs => {
+                let contacts = [];
+
+                docs.forEach(doc => {
+                    let data = doc.data();
+
+                    data.id = doc.id;
+
+                    contacts.push(data);
+                });
+
+                this.trigger('contactschange', docs);
+
+                s(contacts);
+            });
+        });
     }
 }
--- curso-javascript-projeto-whatsapp-clone/src/util/Format.js
+++ curso-javascript-projeto-whatsapp-clone/src/util/Format.js
@@ -13,9 +13,20 @@ export class Format {
         let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 
         if (hours > 0) {
-            return `${hours}:${minutes.toString().padStart(2, '0' )}:${seconds}`;
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds}`;
         } else {
-            return `${minutes}:${seconds.toString().padStart(2, '0' )}`;
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
         }
+    }
+
+    static dateToTime(date, locale = 'pt-BR') {
+        return date.toLocaleTimeString(locale, {
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+    }
+
+    static timeStampToTime(timeStamp) {
+        return (timeStamp && typeof timeStamp.toDate === 'function') ? Format.dateToTime(timeStamp.toDate()) : '';
     }
 }
